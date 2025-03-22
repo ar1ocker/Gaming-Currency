386edff0623e5bfefa202e52e48a83dc18e2947b
ar1ocker <ar1oc@yandex.ru>
2025-03-22 10:24:49 +0300

    Redirect from index to admin site

--- gaming_billing_service/settings/urls.py
+++ gaming_billing_service/settings/urls.py
@@ -1,8 +1,10 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url=reverse_lazy("admin:login"), permanent=False)),
     path("api/currencies/", include("currencies_api.urls")),
     path("actions/currencies/", include("currencies.urls")),
     path("admin/", admin.site.urls),
