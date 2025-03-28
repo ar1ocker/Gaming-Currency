a56c4949ae4969456a54270d855a862aa3b132e1
Ar1ocker <ar1oc@yandex.ru>
2025-03-29 00:00:30 +0300

    Rename celery tasks

--- gaming_billing_service/currencies/tasks.py
+++ gaming_billing_service/currencies/tasks.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 
 @shared_task
-def reject_outdated_currency_transactions():
+def reject_outdated_adjustments():
 
     rejecteds = AdjustmentsService.reject_all_outdated(status_description="Rejected by cron as outdated")
 
@@ -17,7 +17,7 @@ def reject_outdated_currency_transactions():
 
 
 @shared_task
-def rejecting_outdated_transfer_transactions():
+def rejecting_outdated_transfers():
 
     rejecteds = TransfersService.reject_all_outdated(status_description="Rejected by cron as outdated")
 
@@ -25,7 +25,7 @@ def rejecting_outdated_transfer_transactions():
 
 
 @shared_task
-def reject_outdated_exchange_transactions():
+def reject_outdated_exchanges():
 
     rejecteds = ExchangesService.reject_all_outdated(status_description="Rejected by cron as outdated")
 
