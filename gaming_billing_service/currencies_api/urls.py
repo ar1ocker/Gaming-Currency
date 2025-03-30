03bbd94aa126a53e1e84121243100cd5922062d1
Ar1ocker <ar1oc@yandex.ru>
2025-03-30 23:13:38 +0300

    Currencies api url names

--- gaming_billing_service/currencies_api/urls.py
+++ gaming_billing_service/currencies_api/urls.py
@@ -12,21 +12,22 @@ from .views.transfers import TransferConfirmAPI, TransferCreateAPI, TransferReje
 from .views.units import CurrencyUnitsListAPI
 
 urlpatterns = [
-    path("accounts/", CheckingAccountListAPI.as_view()),
-    path("accounts/detail/", CheckingAccountDetailAPI.as_view()),
+    path("accounts/", CheckingAccountListAPI.as_view(), name="checking_accounts_list"),
+    path("accounts/detail/", CheckingAccountDetailAPI.as_view(), name="checking_accounts_detail"),
     #
-    path("units/", CurrencyUnitsListAPI.as_view()),
+    path("units/", CurrencyUnitsListAPI.as_view(), name="currency_units_list"),
     #
-    path("adjustments/", AdjustmentListAPI.as_view()),
-    path("adjustments/create/", AdjustmentCreateAPI.as_view()),
-    path("adjustments/confirm/", AdjustmentConfirmAPI.as_view()),
-    path("adjustments/reject/", AdjustmentRejectAPI.as_view()),
+    path("adjustments/", AdjustmentListAPI.as_view(), name="adjustments_list"),
+    path("adjustments/create/", AdjustmentCreateAPI.as_view(), name="adjustments_create"),
+    path("adjustments/confirm/", AdjustmentConfirmAPI.as_view(), name="adjustments_confirm"),
+    path("adjustments/reject/", AdjustmentRejectAPI.as_view(), name="adjustments_reject"),
     #
-    path("transfers/create/", TransferCreateAPI.as_view()),
-    path("transfers/confirm/", TransferConfirmAPI.as_view()),
-    path("transfers/reject/", TransferRejectAPI.as_view()),
+    path("transfers/create/", TransferCreateAPI.as_view(), name="transfers_create"),
+    path("transfers/confirm/", TransferConfirmAPI.as_view(), name="transfers_confirm"),
+    path("transfers/reject/", TransferRejectAPI.as_view(), name="transfers_reject"),
     #
-    path("exchanges/create/", ExchangeCreateAPI.as_view()),
-    path("exchanges/confirm/", ExchangeConfirmAPI.as_view()),
-    path("exchanges/reject/", ExchangeRejectAPI.as_view()),
+    path("exchanges/create/", ExchangeCreateAPI.as_view(), name="exchanges_create"),
+    path("exchanges/confirm/", ExchangeConfirmAPI.as_view(), name="exchanges_confirm"),
+    path("exchanges/reject/", ExchangeRejectAPI.as_view(), name="exchanges_reject"),
+    #
 ]
