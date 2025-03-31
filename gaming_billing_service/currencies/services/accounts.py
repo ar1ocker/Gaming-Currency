8840840ed4b318f5e0df32c03751518f96ad00ab
ar1ocker <ar1oc@yandex.ru>
2025-03-31 15:18:58 +0300

    Default select related in accounts service

--- gaming_billing_service/currencies/services/accounts.py
+++ gaming_billing_service/currencies/services/accounts.py
@@ -5,14 +5,16 @@ from currencies.models import CheckingAccount, CurrencyUnit, Holder
 class AccountsService:
     @classmethod
     def get_or_create(cls, *, holder: Holder, currency_unit: CurrencyUnit) -> CheckingAccount:
-        return CheckingAccount.objects.get_or_create(
+        return CheckingAccount.objects.select_related("currency_unit").get_or_create(
             holder=holder, currency_unit=currency_unit, defaults={"amount": 0}
         )[0]
 
     @classmethod
     def get(cls, *, holder: Holder, currency_unit: CurrencyUnit) -> CheckingAccount | None:
         try:
-            return CheckingAccount.objects.get(holder=holder, currency_unit=currency_unit)
+            return CheckingAccount.objects.select_related("currency_unit").get(
+                holder=holder, currency_unit=currency_unit
+            )
         except CheckingAccount.DoesNotExist:
             return None
 
