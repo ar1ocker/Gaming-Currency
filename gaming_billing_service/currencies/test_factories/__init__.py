a5aef1a4bb969885f9f5b0a68c20bcb75bc9ad54
ar1ocker <ar1oc@yandex.ru>
2025-04-16 16:08:05 +0300

    __init__.py для новой фабрики

--- gaming_billing_service/currencies/test_factories/__init__.py
+++ gaming_billing_service/currencies/test_factories/__init__.py
@@ -1,3 +1,4 @@
 from .holders import HoldersTestFactory  # noqa
 from .holders_type import HoldersTypeTestFactory  # noqa
 from .units import CurrencyUnitsTestFactory  # noqa
+from .currency_services import CurrencyServicesTestFactory  # noqa
