953b8685fdc02561b29070c48babc4c7931ac66f
Ar1ocker <ar1oc@yandex.ru>
2025-04-24 21:22:28 +0300

    Currencies models: isort

--- gaming_billing_service/currencies/models.py
+++ gaming_billing_service/currencies/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 
