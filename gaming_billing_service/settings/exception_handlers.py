2535bbb07ad3e51370f5c1f05766792a08e7030a
ar1ocker <ar1oc@yandex.ru>
2025-04-26 16:10:16 +0300

    Exception handlers: add http404 handler

--- gaming_billing_service/settings/exception_handlers.py
+++ gaming_billing_service/settings/exception_handlers.py
@@ -1,4 +1,6 @@
-from django.core.exceptions import ValidationError as DjangoValidationError, PermissionDenied
+from django.core.exceptions import PermissionDenied
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.http import Http404
 from rest_framework import exceptions
 from rest_framework.serializers import as_serializer_error
 from rest_framework.views import exception_handler
@@ -7,6 +9,9 @@ from rest_framework.views import exception_handler
 def django_validation_error_exception_handler(exc, ctx):
     if isinstance(exc, DjangoValidationError):
         exc = exceptions.ValidationError(as_serializer_error(exc))
+
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound(str(exc))
 
     if isinstance(exc, PermissionDenied):
         exc = exceptions.PermissionDenied(str(exc))
