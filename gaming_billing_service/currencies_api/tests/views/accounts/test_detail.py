e229ab8a84b002e6dc24fd5c504999583366b509
ar1ocker <ar1oc@yandex.ru>
2025-04-26 17:06:23 +0300

    Accounts detail api: remove holders creating. + tests

--- gaming_billing_service/currencies_api/tests/views/accounts/test_detail.py
+++ gaming_billing_service/currencies_api/tests/views/accounts/test_detail.py
@@ -1,25 +1,27 @@
 from decimal import Decimal
 
 from common.utils import assemble_auth_headers
-from currencies.models import CheckingAccount, Holder
+from currencies.models import CheckingAccount
 from currencies.services import (
     AccountsService,
     CurrencyServicesService,
+    HoldersService,
     HoldersTypeService,
 )
-from currencies.test_factories import CurrencyUnitsTestFactory, HoldersTestFactory
-from currencies.test_factories.currency_services import CurrencyServicesTestFactory
-from currencies_api.models import CurrencyServiceAuth
-from currencies_api.test_factories.currency_service_auth import (
-    CurrencyServiceAuthTestFactory,
+from currencies.test_factories import (
+    CurrencyServicesTestFactory,
+    CurrencyUnitsTestFactory,
+    HoldersTestFactory,
 )
+from currencies_api.models import CurrencyServiceAuth
+from currencies_api.test_factories import CurrencyServiceAuthTestFactory
 from django.test import override_settings
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
 
 @override_settings(ENABLE_HMAC_VALIDATION=False)
-class CheckingAccountDetailTest(APITestCase):
+class AccountDetailAPITest(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.service = CurrencyServicesService.get_default()
@@ -31,11 +33,11 @@ class CheckingAccountDetailTest(APITestCase):
 
         cls.holder_type = HoldersTypeService.get_default()
 
-        cls.holder = HoldersTestFactory.create()
-        cls.currency_unit_1 = CurrencyUnitsTestFactory.create()
-        cls.currency_unit_2 = CurrencyUnitsTestFactory.create()
+        cls.holder = HoldersTestFactory(holder_type=cls.holder_type)
+        cls.currency_unit_1 = CurrencyUnitsTestFactory()
+        cls.currency_unit_2 = CurrencyUnitsTestFactory()
 
-        cls.account = AccountsService.get_or_create(holder=cls.holder, currency_unit=cls.currency_unit_1)
+        cls.account_unit_1 = AccountsService.get_or_create(holder=cls.holder, currency_unit=cls.currency_unit_1)
 
         cls.account_detail_reverse_path = reverse("checking_accounts_detail")
 
@@ -106,10 +108,6 @@ class CheckingAccountDetailTest(APITestCase):
                     enabled=True,
                     create=dict(enabled=False),
                 ),
-                holders=dict(
-                    enabled=True,
-                    create=dict(enabled=True),
-                ),
             )
         )
         CurrencyServiceAuthTestFactory(service=service)
@@ -132,58 +130,37 @@ class CheckingAccountDetailTest(APITestCase):
 
         self.assertEqual(CheckingAccount.objects.count(), 1)
 
-    def test_get_or_create_with_no_holders_create_permissions(self):
-        service = CurrencyServicesTestFactory(
-            permissions=dict(
-                accounts=dict(
-                    enabled=True,
-                    create=dict(enabled=True),
-                ),
-                holders=dict(
-                    enabled=True,
-                    create=dict(enabled=False),
-                ),
-            )
-        )
-        CurrencyServiceAuthTestFactory(service=service)
-
+    def test_get_with_not_exists_holder(self):
         response = self.client.get(
             self.account_detail_reverse_path,
             data=dict(
-                holder_id=self.holder.holder_id,
+                holder_id="not found holder",
                 holder_type=self.holder_type.name,
-                unit_symbol=self.currency_unit_2.symbol,
-                create_if_not_exists=True,
-            ),
-            headers=assemble_auth_headers(service=service),
-        )
-
-        data: dict = response.data  # type: ignore
-
-        self.assertEqual(response.status_code, 403)
-        self.assertIn("Creating is disabled", data["message"], data)
-
-        self.assertEqual(CheckingAccount.objects.count(), 1)
-
-    def test_get_or_create_with_default_holder_type(self):
-        response = self.client.get(
-            self.account_detail_reverse_path,
-            data=dict(
-                holder_id="new_holder_id",
-                unit_symbol=self.currency_unit_2.symbol,  # unit 2
-                create_if_not_exists=True,
+                unit_symbol=self.currency_unit_1.symbol,
             ),
             headers=assemble_auth_headers(service=self.service),
         )
 
-        data: dict = response.data  # type: ignore
+        data = response.data  # type: ignore
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data["holder_id"], "new_holder_id")
-        self.assertEqual(data["holder_type"], HoldersTypeService.get_default().name)
-        self.assertEqual(data["currency_unit"], self.currency_unit_2.symbol)
-        self.assertEqual(Decimal(data["amount"]), Decimal("0.0000"))
-        self.assertTrue("created_at" in data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data.get("message"), "Holder not found")
+        self.assertEqual(HoldersService.list().count(), 1)
 
-        self.assertEqual(CheckingAccount.objects.count(), 2)
-        self.assertEqual(Holder.objects.count(), 2)
+    def test_get_with_not_exists_account(self):
+        holder = HoldersTestFactory()
+
+        response = self.client.get(
+            self.account_detail_reverse_path,
+            data=dict(
+                holder_id=holder.holder_id,
+                holder_type=self.holder_type.name,
+                unit_symbol=self.currency_unit_1.symbol,
+            ),
+            headers=assemble_auth_headers(service=self.service),
+        )
+
+        data = response.data  # type: ignore
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data.get("message"), "Account not found")
--- gaming_billing_service/currencies_api/views/accounts.py
+++ gaming_billing_service/currencies_api/views/accounts.py
@@ -1,10 +1,11 @@
 from currencies.models import CurrencyUnit, HolderType
-from currencies.permissions import AccountsPermissionsService, HoldersPermissionsService
+from currencies.permissions import AccountsPermissionsService
 from currencies.services import AccountsService, HoldersService, HoldersTypeService
 from currencies_api.auth import hmac_service_auth
 from currencies_api.models import CurrencyServiceAuth
 from currencies_api.pagination import LimitOffsetPagination, get_paginated_response
-from rest_framework import serializers, status
+from django.http import Http404
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -36,23 +37,20 @@ class CheckingAccountsDetailAPI(APIView):
         create_if_not_exists: bool = serializer.validated_data["create_if_not_exists"]  # type: ignore
         holder_id: str = serializer.validated_data["holder_id"]  # type: ignore
         holder_type: HolderType = serializer.validated_data["holder_type"]  # type: ignore
-        unit_symbol: CurrencyUnit = serializer.validated_data["unit_symbol"]  # type: ignore
+        currency_unit: CurrencyUnit = serializer.validated_data["unit_symbol"]  # type: ignore
+
+        holder = HoldersService.get(holder_id=holder_id, holder_type=holder_type)
+
+        if holder is None:
+            raise Http404("Holder not found")
 
         if create_if_not_exists:
-            HoldersPermissionsService.enforce_create(permissions=service_auth.service.permissions)
             AccountsPermissionsService.enforce_create(permissions=service_auth.service.permissions)
-
-            holder = HoldersService.get_or_create(holder_id=holder_id, holder_type=holder_type)[0]
-            account = AccountsService.get_or_create(holder=holder, currency_unit=unit_symbol)
+            account = AccountsService.get_or_create(holder=holder, currency_unit=currency_unit)
         else:
-            holder = HoldersService.get(holder_id=holder_id, holder_type=holder_type)
-
-            if holder is None:
-                return Response({"error": "Holder not found"}, status=status.HTTP_404_NOT_FOUND)
-
-            account = AccountsService.get(holder=holder, currency_unit=unit_symbol)
+            account = AccountsService.get(holder=holder, currency_unit=currency_unit)
             if account is None:
-                return Response({"error": "Account not found"}, status=status.HTTP_404_NOT_FOUND)
+                raise Http404("Account not found")
 
         return Response(self.OutputSerializer(dict(account=account, holder=holder)).data)
 
