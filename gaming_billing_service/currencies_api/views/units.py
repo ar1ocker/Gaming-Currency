0cd77f0657c58661ae3003d59e81da31b68ce268
ar1ocker <ar1oc@yandex.ru>
2025-04-29 17:42:26 +0300

    Units list api: add pagination

--- gaming_billing_service/currencies_api/views/units.py
+++ gaming_billing_service/currencies_api/views/units.py
@@ -2,12 +2,15 @@ from currencies.models import CurrencyUnit
 from currencies.permissions import CurrencyUnitsPermissionsService
 from currencies_api.auth import hmac_service_auth
 from currencies_api.models import CurrencyServiceAuth
+from currencies_api.pagination import LimitOffsetPagination, get_paginated_response
 from rest_framework import serializers
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class CurrencyUnitsListAPI(APIView):
+    class Pagination(LimitOffsetPagination):
+        pass
+
     class OutputSerializer(serializers.Serializer):
         symbol = serializers.CharField()
         measurement = serializers.CharField()
@@ -17,4 +20,12 @@ class CurrencyUnitsListAPI(APIView):
 
         CurrencyUnitsPermissionsService.enforce_access(permissions=service_auth.service.permissions)
 
-        return Response(self.OutputSerializer(CurrencyUnit.objects.all(), many=True).data)
+        units = CurrencyUnit.objects.all()
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OutputSerializer,
+            queryset=units,
+            request=request,
+            view=self,
+        )
