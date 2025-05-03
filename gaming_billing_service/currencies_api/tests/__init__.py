b88ee896b136bed5d06dc31b3b331a1e4d77460b
ar1ocker <ar1oc@yandex.ru>
2025-05-03 10:08:59 +0300

    Tests: added __init__.py to all dirs

--- gaming_billing_service/currencies_api/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth.base.test_base import *  # noqa
-from .auth.decorators.test_decorators import *  # noqa
-from .auth.generators.test_battlemetrics_generator import *  # noqa
-from .auth.generators.test_timestamp_generator import *  # noqa
-from .auth.getters.test_regex_header_getter import *  # noqa
-from .auth.getters.test_simple_getter import *  # noqa
-from .views import *  # noqa
