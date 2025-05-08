a3837b9d4963f73752abc60e3ea796b8a39e9207
ar1ocker <ar1oc@yandex.ru>
2025-05-08 10:52:06 +0300

    Settings: formated

--- gaming_billing_service/settings/settings.py
+++ gaming_billing_service/settings/settings.py
@@ -11,7 +11,7 @@ from django.db.backends.postgresql.psycopg_any import IsolationLevel
 getcontext().prec = 20
 getcontext().rounding = ROUND_HALF_UP
 
-# BASE
+# DJANGO BASE
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,7 +32,12 @@ BASE_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_celery_results", "django_celery_beat", "django_filters"]
+THIRD_PARTY_APPS = [
+    "django_celery_results",
+    "django_celery_beat",
+    "django_filters",
+    "rest_framework",
+]
 
 DEBUG_MIDDLEWARE = []
 DEBUG_APPS = []
@@ -46,7 +51,6 @@ elif DEBUG:
 
 
 LOCAL_APPS = [
-    "rest_framework",
     "currencies",
     "currencies_api",
 ]
@@ -84,10 +88,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "settings.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+# DJANGO DATABASE
 
 DATABASES = {
     "default": {
@@ -104,12 +105,17 @@ DATABASES = {
 }
 
 if "test" in sys.argv and "--no-input" not in sys.argv:
-    print("[ ! ] Redefining the standard database to sqlite for local testing, check settings/settings.py")
+    print(
+        "[ ! ] Redefining the standard database to sqlite for local testing, check settings/settings.py, "
+        "add --no-input to disable redefining"
+    )
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
     }
 
+
+# DJANGO AUTH
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -126,69 +132,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = "/admin/login/"
 
-LANGUAGE_CODE = "en-us"
+# DJANGO SESSIONS
 
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# DEBUG TOOLBAR
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
-    "PROFILER_MAX_DEPTH": 10,
-}
-
-# HMAC
-ENABLE_HMAC_VALIDATION = config["HMAC"]["ENABLE"]
-SERVICE_HEADER = "X-SERVICE"
-
-BATTLEMETRICS_SIGNATURE_REGEX = r"(?<=s=)\w+(?=,|\Z)"
-BATTLEMETRICS_TIMESTAMP_REGEX = r"(?<=t=)[\w\-:.+]+(?=,|\Z)"
-HMAC_SIGNATURE_HEADER = "X-SIGNATURE"
-HMAC_TIMESTAMP_HEADER = "X-SIGNATURE_TIMESTAMP"
-HMAC_HASH_TYPE = "sha256"
-
-HMAC_TIMESTAMP_DEVIATION = timedelta(seconds=config["HMAC"]["TIMESTAMP_DEVIATION"])
-
-# Sessions
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-REST_FRAMEWORK = {"EXCEPTION_HANDLER": "settings.exception_handlers.django_validation_error_exception_handler"}
-
-LOGIN_URL = "/admin/login/"
-
-# Currency transactions
-DEFAULT_AUTO_REJECT_TIMEDELTA = timedelta(seconds=config["CURRENCY_TRANSACTIONS"]["DEFAULT_AUTO_REJECT_SECONDS"])
-DEFAULT_AUTO_REJECT_SECONDS = DEFAULT_AUTO_REJECT_TIMEDELTA.total_seconds()
-CURRENCY_DEFAULT_HOLDER_TYPE_SLUG = "player"
-ADMIN_SITE_SERVICE_NAME = "admin-site"
-
-# CELERY
-CELERY_RESULT_EXPIRES = timedelta(days=config["CELERY"]["RESULT_EXPIRES_DAYS"])
-CELERY_BROKER_URL = config["CELERY"]["BROKER_URL"]
-CELERY_RESULT_BACKEND = "django-db"
-CELERY_RESULT_EXTENDED = True
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
-CELERY_WORKER_PREFETCH_MULTIPLIER = 1
-CELERY_WORKER_CONCURRENCY = 2
-
-# Logging
+# DJANGO LOGGING
 
 if "test" not in sys.argv:
     LOGGING = {
@@ -197,7 +148,8 @@ if "test" not in sys.argv:
         "formatters": {
             "machine_readable": {
                 "format": (
-                    "{levelname} | {asctime} | {module} | {name} | {process:d} | {thread:d} | {ip} | {status_code} | {message} | {params}"
+                    "{levelname} | {asctime} | {module} | {name} | {process:d} | "
+                    "{thread:d} | {ip} | {status_code} | {message} | {params}"
                 ),
                 "style": "{",
                 "datefmt": "%Y-%m-%d %H:%M:%S",
@@ -224,3 +176,64 @@ if "test" not in sys.argv:
             "level": "WARNING",
         },
     }
+
+# DJANGO I18N
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+# DJANGO STATIC FILES
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+# DJANGO ETC
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF
+
+REST_FRAMEWORK = {"EXCEPTION_HANDLER": "settings.exception_handlers.django_validation_error_exception_handler"}
+
+# DEBUG TOOLBAR
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
+    "PROFILER_MAX_DEPTH": 10,
+}
+
+# HMAC
+
+ENABLE_HMAC_VALIDATION = config["HMAC"]["ENABLE"]
+SERVICE_HEADER = "X-SERVICE"
+
+BATTLEMETRICS_SIGNATURE_REGEX = r"(?<=s=)\w+(?=,|\Z)"
+BATTLEMETRICS_TIMESTAMP_REGEX = r"(?<=t=)[\w\-:.+]+(?=,|\Z)"
+HMAC_SIGNATURE_HEADER = "X-SIGNATURE"
+HMAC_TIMESTAMP_HEADER = "X-SIGNATURE_TIMESTAMP"
+HMAC_HASH_TYPE = "sha256"
+
+HMAC_TIMESTAMP_DEVIATION = timedelta(seconds=config["HMAC"]["TIMESTAMP_DEVIATION"])
+
+
+# Currency transactions
+
+DEFAULT_AUTO_REJECT_TIMEDELTA = timedelta(seconds=config["CURRENCY_TRANSACTIONS"]["DEFAULT_AUTO_REJECT_SECONDS"])
+DEFAULT_AUTO_REJECT_SECONDS = DEFAULT_AUTO_REJECT_TIMEDELTA.total_seconds()
+CURRENCY_DEFAULT_HOLDER_TYPE_SLUG = "player"
+ADMIN_SITE_SERVICE_NAME = "admin-site"
+
+# CELERY
+
+CELERY_RESULT_EXPIRES = timedelta(days=config["CELERY"]["RESULT_EXPIRES_DAYS"])
+CELERY_BROKER_URL = config["CELERY"]["BROKER_URL"]
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_RESULT_EXTENDED = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_WORKER_PREFETCH_MULTIPLIER = 1
+CELERY_WORKER_CONCURRENCY = 2
