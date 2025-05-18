a2e053c87f02b38c8005d7399b75b3fbbeda37ce
Ar1ocker <ar1oc@yandex.ru>
2025-05-18 12:48:01 +0300

    Postgres backup: transferring a backup to a separate file

--- services/postgres_backup/internal/application/application.go
+++ services/postgres_backup/internal/application/application.go
@@ -1,18 +1,14 @@
 package application
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"time"
 
 	"postgres_backup/internal/backup"
 
 	"github.com/go-telegram/bot"
-	"github.com/go-telegram/bot/models"
 )
 
 func NewApplication(ctx context.Context, config *Config, bot *bot.Bot) *Application {
@@ -31,57 +27,6 @@ func (app *Application) RunApplication(ctx context.Context) {
 
 	go app.BackupsProcessing(ctx)
 	app.backupExecutor.RunBackupAfter(time.Second * 3)
-}
-
-func (app *Application) BackupsProcessing(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case result, ok := <-app.backupChan:
-			if !ok {
-				log.Println("The backup channel has been closed")
-				return
-			}
-
-			if result.Err != nil {
-				log.Println(result.Err)
-				app.SendMessageToAllAdmins(ctx, result.Err.Error())
-				continue
-			}
-
-			fileData, err := os.ReadFile(result.FilePath)
-			if err != nil {
-				msg := fmt.Sprintf("Error on opening file %v, %v\n", result.FilePath, err)
-				log.Println(msg)
-				app.SendMessageToAllAdmins(ctx, msg)
-			}
-
-			for _, chatID := range app.config.backupChatIDs {
-				_, err := app.b.SendDocument(
-					ctx,
-					&bot.SendDocumentParams{ChatID: chatID, Document: &models.InputFileUpload{
-						Filename: filepath.Base(result.FilePath),
-						Data:     bytes.NewReader(fileData),
-					}})
-				if err != nil {
-					msg := fmt.Sprintf("Error on send file %s to chat %s", result.FilePath, chatID)
-					log.Println(msg)
-					app.SendMessageToAllAdmins(ctx, msg)
-				}
-				log.Printf("Sended backup %v to chat %v\n", filepath.Base(result.FilePath), chatID)
-			}
-
-			err = os.Remove(result.FilePath)
-			if err != nil {
-				msg := fmt.Sprintf("Error on delete log file %v", err)
-				log.Println(msg)
-				app.SendMessageToAllAdmins(ctx, msg)
-			} else {
-				log.Printf("Sended file %v has been deleted\n", result.FilePath)
-			}
-		}
-	}
 }
 
 func (app *Application) SendMessageToAllAdmins(ctx context.Context, message string) {
---
+++ services/postgres_backup/internal/application/backup.go
@@ -0,0 +1,66 @@
+package application
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (app *Application) BackupsProcessing(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case result, ok := <-app.backupChan:
+			if !ok {
+				log.Println("The backup channel has been closed")
+				return
+			}
+
+			if result.Err != nil {
+				log.Println(result.Err)
+				app.SendMessageToAllAdmins(ctx, result.Err.Error())
+				continue
+			}
+
+			fileData, err := os.ReadFile(result.FilePath)
+			if err != nil {
+				msg := fmt.Sprintf("Error on opening file %v, %v\n", result.FilePath, err)
+				log.Println(msg)
+				app.SendMessageToAllAdmins(ctx, msg)
+				continue
+			}
+
+			for _, chatID := range app.config.backupChatIDs {
+				_, err := app.b.SendDocument(
+					ctx,
+					&bot.SendDocumentParams{ChatID: chatID, Document: &models.InputFileUpload{
+						Filename: filepath.Base(result.FilePath),
+						Data:     bytes.NewReader(fileData),
+					}})
+				if err != nil {
+					msg := fmt.Sprintf("Error on send file %s to chat %s", result.FilePath, chatID)
+					log.Println(msg)
+					app.SendMessageToAllAdmins(ctx, msg)
+					continue
+				}
+				log.Printf("Sended backup %v to chat %v\n", filepath.Base(result.FilePath), chatID)
+			}
+
+			err = os.Remove(result.FilePath)
+			if err != nil {
+				msg := fmt.Sprintf("Error on delete log file %v", err)
+				log.Println(msg)
+				app.SendMessageToAllAdmins(ctx, msg)
+			} else {
+				log.Printf("Sended file %v has been deleted\n", result.FilePath)
+			}
+		}
+	}
+}
